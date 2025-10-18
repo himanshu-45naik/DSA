5b1c43396b24d9bd3eeafa6ba55846cf514d8300
himanshu-45naik <himanshugit45@gmail.com>
2025-10-18 11:09:33 +0530

    Brute Force Solution

---
+++ 2pointers/ContainerWithMostWater.py
@@ -0,0 +1,24 @@
+# 11. Container With Most Water.
+
+# you are given an integer array height of length n
+# There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+# Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        
+        # Bruteforce Approach
+        # Check for all walls, to built an intuition.
+        contained = 0
+        n = len(height)
+
+        for i in range (n):
+            for j in range(i + 1, n):
+                smaller_wall = min(height[i], height[j])
+                contained = max(contained, (j - i) * smaller_wall) 
+
+        return contained
+
+s = Solution()
+print(s.maxArea([1,8,6,2,5,4,8,3,7]))    
+print(s.maxArea([1,1]))
