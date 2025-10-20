c44681424de0c4f7fdb265f66afc64362e75f3ab
himanshu-45naik <himanshugit45@gmail.com>
2025-10-20 10:30:51 +0530

    Max number of k-sum pairs Optimised  Solution

--- 2pointers/prblm_1679.py
+++ 2pointers/prblm_1679.py
@@ -12,22 +12,54 @@ class Solution:
         # Remove those which are equal to k.
         # Keep track of the remove operation.        
 
+        # n = len(nums)
+        # used = [False] * n  # track if an element is already used
+        # count = 0
+
+        # for i in range(n):
+        #     if used[i]:
+        #         continue
+        #     for j in range(i + 1, n):
+        #         if used[j]:
+        #             continue
+        #         if nums[i] + nums[j] == k:
+        #             count += 1
+        #             used[i] = used[j] = True  # mark both as used
+        #             break  # move to next i once a valid pair is found
+
+        # return count
+
+        # Optimized solution
+
+        nums.sort()
         n = len(nums)
-        used = [False] * n  # track if an element is already used
+        l = 0
+        r = n-1
+        sum = 0
+        used = [False] * n
         count = 0
 
-        for i in range(n):
-            if used[i]:
-                continue
-            for j in range(i + 1, n):
-                if used[j]:
-                    continue
-                if nums[i] + nums[j] == k:
-                    count += 1
-                    used[i] = used[j] = True  # mark both as used
-                    break  # move to next i once a valid pair is found
+        while l < r:
+            if used[l]:
+                l +=1
+            if used[r]:
+                r-=1
+
+            sum = nums[l] + nums[r]
+
+            if sum == k:
+                used[l] = True
+                used[r] = True
+                count +=1
+                l+=1
+                r-=1
+            elif sum < k:
+                l+=1
+            else:
+                r-=1
 
         return count
+
     
 s = Solution()
 print(s.maxOperations([1,2,3,4], 5))
