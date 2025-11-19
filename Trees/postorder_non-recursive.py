d1db417d50ea733ed8dc9c76b5fd5c1b45efb735
himanshu-45naik <himanshugit45@gmail.com>
2025-11-19 18:38:42 +0530

    2 stack implementation

--- Trees/postorder_non-recursive.py
+++ Trees/postorder_non-recursive.py
@@ -21,5 +21,28 @@ class Solution:
         
         # Approach-
         # Traversal in postorder tree - Left-> Right -> Root
-        # We will use 2 stacks -
+        # We will follow to approach similar to  Preorder traversal here. Remember preorder is Root-Left-Right
+        # We will use modified preorder : Root-Right-Left
+        # In reverse it is a Postorder traversal.
+        # We will use 2 stacks - Stk1 is the modified preorder traversal tool
+        # Stk2 is used to store history for keeping track of nodes already visited.
         # In one stack we will push the 
+
+        stk1 = []
+        stk2 = []
+
+        if not root:
+            return stk2
+        
+        stk1.append(root)
+
+        while stk1:
+            root = stk1.pop()
+            stk2.append(root)
+
+            if root.left:
+                stk1.append(root.left)
+            if root.right:
+                stk1.append(root.right)
+            
+        return stk2[::-1]
